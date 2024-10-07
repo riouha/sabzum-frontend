f925a61a51f5b9524e1e289d12bac9dbe51eccb2
4riouha <4riuha@gmail.com>
2024-10-07 18:38:23 +0330

    .

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
+// const dns = require('dns');
+// dns.setDefaultResultOrder('ipv4first');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    domains: ['http://87.248.150.89:4100'],
     remotePatterns: [
       { protocol: 'http', hostname: '127.0.0.1' },
       { protocol: 'http', hostname: '87.248.150.89' },
