cc214655910739ea415d9df6161166421f12f037
4riouha <4riuha@gmail.com>
2024-10-07 18:53:02 +0330

    .

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['http://87.248.150.89:4100'],
+    domains: ['87.248.150.89'],
     remotePatterns: [
       { protocol: 'http', hostname: '127.0.0.1' },
       { protocol: 'http', hostname: '87.248.150.89' },
