063387856c6959c836d4ec0b5d57a01de3b4233a
4riouha <4riuha@gmail.com>
2024-10-07 19:54:15 +0330

    .

--- next.config.js
+++ next.config.js
@@ -6,10 +6,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['87.248.150.89'],
+    domains: ['87.248.150.89', 'api.sabzum.ir', 'www.api.sabzum.ir'],
     remotePatterns: [
       { protocol: 'http', hostname: '127.0.0.1' },
       { protocol: 'http', hostname: '87.248.150.89' },
+      { protocol: 'http', hostname: 'sabzum.ir' },
+      { protocol: 'http', hostname: 'api.sabzum.ir' },
     ],
   },
 };
